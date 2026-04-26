69119bb4d96d47de58f2d920e99fe7f039d7d00f
xhx <haoxiang_xia@outlook.com>
2026-04-27 05:22:38 +0800

    fix: 修复了 vite.config.js 中的路径问题

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: path.resolve(__dirname),
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8000'
